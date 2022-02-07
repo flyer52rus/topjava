92050b4653226b7cdaf45c7549327f77550fa86a
flyer52rus <flyercombat@yandex.ru>
2022-02-07 09:15:20 +0300

    add getters in MealTo released class MealServlet and meals.jsp

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,10 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.datasource.DataSource;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,16 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
+    private final int caloriesPerDay = 2000;
+    List<Meal> meals;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
-
-        response.sendRedirect("meals.jsp");
+        meals = new DataSource().getMeals();
+        List<MealTo> mealsTo = MealsUtil.filteredByStreams(meals, LocalTime.of(0, 0), LocalTime.of(23, 59, 59, 999999999), caloriesPerDay);
+        response.setContentType("text/html;charset=utf-8");
+        System.out.println(mealsTo);
+        request.setAttribute("mealsTo", mealsTo);
+        getServletContext().getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html lang="ru">
 <head>
@@ -7,5 +8,21 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<br>
+<table border="3">
+    <tr>
+        <th >Date</th>
+        <th >Description</td>
+        <th >Calories</td>
+    </tr>
+    <c:forEach items="${mealsTo}" var="mealsTo">
+        <tr style="color: ${mealsTo.excess ? 'red' : 'green'}">
+            <td> ${mealsTo.dateTime} </td>
+            <td> ${mealsTo.description} </td>
+            <td> ${mealsTo.calories} </td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
+
